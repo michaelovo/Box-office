716a05fc280e5ee2ceb4429e7de0cfe3bade0b86
michaelovo <emikeovo@gmail.com>
2022-02-12 13:00:55 +0100

    Added styling to display movie contents/details

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,7 +18,18 @@
 
     <div class="movie-list">
       <div class="movies" v-for="movie in movies" :key="movie.imdbID">
-        {{movie.Title}}
+        <!-- {{movie.Title}} -->
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+        <div class="product-image">
+          <img :src="movie.Poster" alt="Movie Poster"/>
+          <div class="type">{{movie.Type}}</div>
+        </div>
+
+        <div class="details">
+          <p class="year">{{movie.Year}}</p>
+          <h3>{{movie.Title}}</h3>
+        </div>
+        </router-link>
         </div>
       
     </div>
@@ -61,8 +72,7 @@
   }
 </script>
 
-<style scoped>
- 
+<style scoped> 
   .feature-card{
     position: relative;
 
@@ -137,5 +147,54 @@
   }
   :active{
     background-color: #3b8070;
+  }
+  .movie-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px
+  }
+  .movie{
+    max-width: 50%;
+    flex: 1 1 50%;
+    padding: 16px 8px;
+  }
+  .movie-link{
+    display: flex;
+    flex-direction: column;
+    height:100%;
+  }
+  .product-image{
+    display: block;
+    position:relative;
+  }
+  img{
+    display: block;
+    width: 100%;
+    height: 275px;
+    object-fit: cover;
+  }
+  .type{
+    position: absolute;
+    padding: 8px 16px;
+    background-color: #42b883;
+    color: #fff;
+    bottom: 16px;
+    left: 0px;
+    text-transform:capitalize;
+  }
+  .details {
+    background-color: #496583;
+    padding:16px 8px;
+    flex: 1 1 100%;
+    border-radius: 0px 0px 8px 8px;
+  }
+  .year {
+    color:#aaa;
+    font-size: 14px;
+  }
+  h3 {
+    font: size 18px;
+    color:#fff;
+    font-weight: 600;
   }
 </style>
