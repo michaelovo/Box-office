cc039683d9d4f48ad8d319fc8ef8bf7d9ba17755
michaelovo <emikeovo@gmail.com>
2022-02-12 05:28:45 +0100

    Set-up and configured movies home page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1><span>Vue</span>Movies</h1>
+    </router-link>
+  </header>
+
+  <main>
+    <router-view/>
+  </main>
+
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
+  * {
+    box-sizing: border-box;
+    margin:0;
+    padding:0;
+    font-family: 'Fira Sans', sans-serif;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  ::selection {
+    /* color: red; */
+    background: #42b883
+  }
+  body {
+    background: color #35495e;
+  }
+  a {
+    text-decoration: none;
+  }
+  header {
+    display : flex;
+    align-items : center;
+    justify-content : center;
+    padding: 10px 16px;
+    background-color: #2c3d4a;
+    box-shadow: 0px 0px 6px rgba(0, 0, 0,0.1);
+  }
+  h1 {
+    color: #fff;
+    font-size: 28px;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  }
+  span {
+    color: #42b883;
+  }
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="feature-card">
+      <router-link to="/movie/tt040951">
+       <img src="https://lumiere-a.akamaihd.net/v1/images/p_aladdin2019_17638_d53b09e6.jpeg" alt="Naruto" class="featured-img" />
+      <div class="detail">
+        <h3>Naruto</h3>
+        <p>
+          Placeholder.com is a free image placeholder service for web designers, serving billions of images per year.
+        </p>
+      </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    
   }
-}
 </script>
+
+<style scoped>
+ 
+  .feature-card{
+    position: relative;
+
+  }
+  .featured-img{
+    display: block;
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    position: relative;
+    z-index: 0;
+  }
+
+  .detail{
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 16px;
+    z-index: 1;
+  }
+  h3{
+    color:#fff;
+    margin-bottom:16px;
+  }
+  p{
+    color:#fff;
+  }
+</style>
