9ff3a714d039d6168506858d44398dcf1eefbe9a
michaelovo <emikeovo@gmail.com>
2022-02-12 12:08:21 +0100

    Added methods,search functionalities and components to retreived and display searched movie details from the OMDBAPI

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import MovieDetail from '../views/MovieDetail.vue'
 
 const routes = [{
-    path: '/',
-    name: 'Home',
-    component: Home
-}, ]
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/movie/:id',
+        name: 'Movie Details',
+        component: MovieDetail
+    },
+]
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,13 +11,52 @@
       </div>
       </router-link>
     </div>
+    <form @submit.prevent="searchMovies()" class="search-box">
+      <input type="text" placeholder="What are you looking for?" v-model="searchData"/>
+      <input type="submit" value="Search">
+    </form>
+
+    <div class="movie-list">
+      <div class="movies" v-for="movie in movies" :key="movie.imdbID">
+        {{movie.Title}}
+        </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
   // @ is an alias to /src
+  import {ref} from 'vue';
+  import env from '@/env.js';
 
   export default {
+    setup(){
+      const searchData = ref("");
+      const movies = ref([]);
+
+      const searchMovies = () =>{
+        if(searchData.value != ""){
+          fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${searchData.value}`)
+          .then(response => response.json())
+          .then(data =>{
+            movies.value = data.Search;
+            searchData.value = "";
+
+            // console.log(data);
+            
+          });
+
+        }
+      }
+      return {
+
+        searchData,
+        movies,
+        searchMovies
+      }
+
+    }
     
   }
 </script>
@@ -32,7 +71,7 @@
     display: block;
     width: 100%;
     height: 300px;
-    object-fit: cover;
+    object-fit:cover;
     position: relative;
     z-index: 0;
   }
@@ -52,5 +91,51 @@
   }
   p{
     color:#fff;
+  }
+
+  .search-box{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+  }
+  input{
+    display:block;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+  }
+  input[type="text"]{
+    width: 100%;
+    color: #000;;
+    background: color #496583;
+    font-size: 20px;
+    padding: 10px 16px;
+    border-radius:8px;
+    margin-bottom: 15px;
+    transition: 0.4s;
+  }
+  ::placeholder{
+    color: #686464;
+  }
+  :focused {
+    box-shadow: 0 3px 6px rgba(0,0,0,0.2);
+
+  }
+  input[type="submit"]{
+    width: 100%;
+    max-width: 300px;
+    background-color: #42b883;
+    padding:16px;
+    border-radius: 8px;
+    color: #fff;
+    font-size: 20px;
+    text-transform: uppercase;
+    transition: 00.4s;
+  }
+  :active{
+    background-color: #3b8070;
   }
 </style>
